820af3cdc6e39ec695a8a889b54ed261d0f9687f
Сергей Черевко <s.cherevko@emzior.ru>
2019-02-01 14:36:42 +0300

    task1 done

---
+++ task1/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+// Для чисел
+template <typename T>
+T Sqr(T & t)
+{
+    return t * t;
+}
+
+// Специализация для vector
+template <>
+vector <int> Sqr(vector<int> &v)
+{
+    int i = 0;
+    for(int x : v){
+        v[i] = x * x;
+        i++;
+    }
+    return v;
+}
+
+// Специализация для pair
+template <>
+pair <int, int> Sqr(pair<int, int> &p)
+{
+    p.first *= p.first;
+    p.second *= p.second;
+    return p;
+}
+
+// Специализация для map
+template <>
+map <int, pair<int, int>>Sqr(map <int, pair<int, int>> &m)
+{
+    for (auto& x : m)
+    {
+        x.second = Sqr(x.second);  
+    }
+    return m;
+}
+
+int main(int argc, char** argv) {
+// Пример вызова функции
+// int    
+    int a = 2;
+    cout << "int a = " << a << " Sqr(a) = " << Sqr(a) << endl;
+// double
+    double b = 2;
+    cout << "double b = " << b << " Sqr(b) = " << Sqr(b) 
+            << endl << endl;
+// vector
+    vector<int> v = {1, 2, 3};
+    cout << "vector:";
+    for (int x : Sqr(v)) {
+      cout << ' ' << x;
+    }
+    cout << endl << endl;
+// pair    
+    pair<int, int> pr = {3, 4};
+    cout << "pairs:" << endl;
+    cout << "{" << pr.first << ", " << pr.second << "}" << endl;
+    pr = Sqr(pr);
+    cout << "Sqr pairs:" << endl;
+    cout << "{" << pr.first << ", " << pr.second << "}" 
+            << endl << endl;
+// map    
+    map<int, pair<int, int>> map_of_pairs = {
+      {4, {2, 2}},
+      {7, {4, 3}}
+    };
+    cout << "map of pairs:" << endl;
+    for (const auto& x : Sqr(map_of_pairs)) {
+      cout << x.first << ' ' << x.second.first << ' ' 
+              << x.second.second << endl;
+    }
+
+    return 0;
+}
+
